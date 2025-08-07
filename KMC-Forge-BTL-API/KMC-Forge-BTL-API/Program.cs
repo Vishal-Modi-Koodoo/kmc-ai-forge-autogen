39796588728f051728d92fd661ea1e1dc4796d94
Monish Koyott <monish.koyott@koodoo.io>
2025-08-07 15:59:27 +0530

    initial setup

--- KMC-Forge-BTL-API/KMC-Forge-BTL-API/Program.cs
+++ KMC-Forge-BTL-API/KMC-Forge-BTL-API/Program.cs
@@ -1,3 +1,6 @@
+using KMC_AI_Forge_BTL_Agent.Contracts;
+using KMC_AI_Forge_BTL_Agent.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IDocumentStorageService, DocumentStorageService>();
+builder.Services.AddTransient<IDocumentRetrievalService, DocumentRetrievalService>();
+builder.Services.AddTransient<IPortfolioValidationService, PortfolioValidationService>();
+builder.Services.AddTransient<IAgentRuntime, AgentRuntime>();
+
+// Add custom logging
+builder.Services.AddSingleton<ILoggerFactory, CustomLoggerFactory>();
+builder.Services.AddSingleton<IExternalScopeProvider, LoggerExternalScopeProvider>();
 
 var app = builder.Build();
 
@@ -21,5 +33,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.Run();
